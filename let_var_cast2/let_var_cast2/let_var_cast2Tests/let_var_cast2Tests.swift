f650fc722388e26f2bc56131093dd0d814daa34a
Shingo Ogata <sng1974i@gmail.com>
2015-12-06 22:25:39 +0900

    -61
    * 2-2完了

---
+++ let_var_cast2/let_var_cast2/let_var_cast2Tests/let_var_cast2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  let_var_cast2Tests.swift
+//  let_var_cast2Tests
+//
+//  Created by shingo on 2015/12/06.
+//  Copyright © 2015年 shingo ogata. All rights reserved.
+//
+
+import XCTest
+@testable import let_var_cast2
+
+class let_var_cast2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
