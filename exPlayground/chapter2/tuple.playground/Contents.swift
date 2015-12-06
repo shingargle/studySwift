122b3ccfb0b27a629fdfa23044a5b0a5289d8135
Shingo Ogata <shingo.ogata@jrits.ricoh.co.jp>
2015-12-07 08:59:37 +0900

    62まで

---
+++ exPlayground/chapter2/tuple.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let product = ("Swift" , 2014)
+var kingaku = (1000, 80)
+var guest = ("shingo", "tako", 0987)
+
+let products:(String, Int) = ("Swift" , 2014)
+
+let data:(Int , Int) = kingaku
+let (zeinuki, _) = data
+print(zeinuki)
