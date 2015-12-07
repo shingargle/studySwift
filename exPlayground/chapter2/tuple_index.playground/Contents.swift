c5cce827446c3260ef118ce0e39a35bb6b1e55bb
Shingo Ogata <sng1974i@gmail.com>
2015-12-07 23:23:24 +0900

    -69

---
+++ exPlayground/chapter2/tuple_index.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let kingaku = (1000, 80)
+let price = kingaku.0
+let tax = kingaku.1
+print(price+tax)
+
+var user = ("鈴木" , 29)
+user.1 = 30
+print(user)
---
+++ exPlayground/chapter2/tuple_label.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let kingaku = (price:1000, tax:80)
+let seikyuugaku = kingaku.price + kingaku.tax
+print(seikyuugaku)
+
+var user:(name:String, age:Int, isPass:Bool)
+user.name = "高田"
+user.age = 32
+user.isPass = true
+
+print(user)
+
+
+let aa = 3 + 2/3
+
+let a = true
+let not = !a
