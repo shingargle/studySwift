fbf02b23a94ee7473ada7d7c34cbc426814c5f64
Shingo Ogata <shingo.ogata@jrits.ricoh.co.jp>
2015-12-08 08:54:40 +0900

    section2-4 まで完了

---
+++ exPlayground/chapter2/range_Range.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+for i in (1 ..< 5)
+{
+    print("\(i) 回目")
+}
+
+for i in (1...5)
+{
+    print("\(i) 回目")
+}
+
+let theRange = Range(start:5, end:10)
+for i in theRange
+{
+    print("\(i) 回目")
+}
