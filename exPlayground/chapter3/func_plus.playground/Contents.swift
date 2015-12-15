9f4d3b6a1a75b72f96855f5d165c505cca6fcbcf
Shingo Ogata <shingo.ogata@jrits.ricoh.co.jp>
2015-12-15 09:26:42 +0900

    session3-2 途中まで

---
+++ exPlayground/chapter3/func_plus.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func plus(a:Double, b:Double, c:Double) -> Double {
+    return a + b + c
+}
+
+let ans = plus(5, b: 6, c: 7)
+print(ans)
---
+++ exPlayground/chapter3/func_twice.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func twice(num:Double) -> Double {
+    return num * 2
+}
+
+let ans = twice(14)
+print(ans)
