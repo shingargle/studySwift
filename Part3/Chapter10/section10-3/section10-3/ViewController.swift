6a1849427d9a224d9b75d78b6d0c4fc798fefac9
Shingo Ogata <sng1974i@gmail.com>
2016-01-23 22:22:23 +0900

    Chapter10 完了

---
+++ Part3/Chapter10/section10-3/section10-3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  section10-3
+//
+//  Created by shingo on 2016/01/22.
+//  Copyright © 2016年 shingo ogata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
